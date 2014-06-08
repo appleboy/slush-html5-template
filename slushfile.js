26e08d510147081c8a40dc4aee53c29b491ebf13
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-06-08 15:26:44 +0800

    initial slush config.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ slushfile.js
@@ -0,0 +1,33 @@
+'use strict'
+
+var gulp = require('gulp'),
+  install = require('gulp-install'),
+  conflict = require('gulp-conflict'),
+  template = require('gulp-template'),
+  rename = require('gulp-rename'),
+  inquirer = require('inquirer');
+
+gulp.task('default', function (done) {
+  inquirer.prompt([
+    {type: 'input', name: 'name', message: 'Give your app a name', default: gulp.args.join(' ')},
+    {type: 'confirm', name: 'moveon', message: 'Continue?'}
+  ],
+  function (answers) {
+    if (!answers.moveon) {
+      return done();
+    }
+    gulp.src(__dirname + '/templates/**')
+      .pipe(template(answers))
+      .pipe(rename(function (file) {
+        if (file.basename[0] === '_') {
+          file.basename = '.' + file.basename.slice(1);
+        }
+      }))
+      .pipe(conflict('./'))
+      .pipe(gulp.dest('./'))
+      .pipe(install())
+      .on('finish', function () {
+        done();
+      });
+  });
+});
