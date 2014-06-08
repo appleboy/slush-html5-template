c843c10f3aecd1cc7bb12a2c3e6286b6940f30f8
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-06-08 16:09:17 +0800

    ignore sass underline file.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- slushfile.js
+++ slushfile.js
@@ -19,7 +19,7 @@ gulp.task('default', function (done) {
     gulp.src(__dirname + '/templates/**')
       .pipe(template(answers))
       .pipe(rename(function (file) {
-        if (file.basename[0] === '_') {
+        if (file.basename[0] === '_' && file.extname !== '.scss' ) {
           file.basename = '.' + file.basename.slice(1);
         }
       }))
