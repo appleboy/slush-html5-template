e94d9449e73c78dfcae752e889273eaa5c4e51d3
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-06-17 11:18:25 +0800

    choose css framework.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- slushfile.js
+++ slushfile.js
@@ -8,14 +8,35 @@ var gulp = require('gulp'),
   inquirer = require('inquirer');
 
 gulp.task('default', function (done) {
-  inquirer.prompt([
-    {type: 'input', name: 'name', message: 'Give your app a name', default: gulp.args.join(' ')},
-    {type: 'confirm', name: 'moveon', message: 'Continue?'}
+  inquirer.prompt([{
+    type: 'input',
+    name: 'name',
+    message: 'Give your app a name',
+    default: gulp.args.join(' ')
+  }, {
+    type: 'list',
+    name: 'cssFramework',
+    message: 'What framework of css do you want?',
+    choices: [{
+        name: "Sass: Syntactically Awesome Style Sheets.",
+        value: "includeSass"
+      }, {
+        name: "Compass is an open-source CSS Authoring Framework.",
+        value: "includeCompass"
+      }
+    ],
+    default: 'sass'
+  }, {
+    type: 'confirm',
+    name: 'moveon',
+    message: 'Continue?'}
   ],
   function (answers) {
     if (!answers.moveon) {
       return done();
     }
+    answers.includeSass = (answers.cssFramework === 'includeSass') ? true : false;
+    answers.includeCompass = (answers.cssFramework === 'includeCompass') ? true : false;
     gulp.src(__dirname + '/templates/**')
       .pipe(template(answers))
       .pipe(rename(function (file) {
